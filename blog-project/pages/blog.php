683ebe2cc6035c309854ec9665e44bb4cea40a9b
Samuelkr <62068089+Samuel-Clover@users.noreply.github.com>
2020-08-14 15:20:07 -0300

    Add files via upload

---
+++ blog-project/pages/blog.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Blog</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/blog.css">
+    <script src="https://kit.fontawesome.com/24989b391d.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:ital,wght@0,400;1,100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;1,100;1,200;1,500&family=Source+Sans+Pro:ital,wght@0,300;1,200&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <section>
+        <div class="center left w33">
+            <div class="artigos-em-alta">
+                <div class="artigos-em-alta-h2">
+                    <h2><i class="fas fa-sort-amount-up-alt"></i> Artigos em Alta</h2>
+                </div><!-- artigos-em-alta-h2 -->
+                <a href=""><i class="fas fa-arrow-circle-right fans"></i> Coronavírus: Brasil registra 58.509 casos e 4.016 mortes</a>
+                <a href=""><i class="fas fa-arrow-circle-right fans"></i> Quatro em cada 10 brasileiros pratica atividades físicas regularmente</a>
+                <a href=""><i class="fas fa-arrow-circle-right fans"></i> Os 10 segredos sobre como fazer Email Marketing</a>
+                <a href=""><i class="fas fa-arrow-circle-right fans"></i> Como criar campanhas de Google Adwords</a>  
+            </div><!-- artigos-em-alta -->
+            <div class="categoria-artigos">
+                <div class="categoria-artigos-h2">
+                    <h2><i class="far fa-bookmark"></i> Categorias</h2>
+                </div> <!-- categoria-artigos-h2 -->
+                <a href=""><i class="fas fa-bullseye"></i> Coronavirus</a>   
+                <a href=""><i class="fas fa-bullseye"></i> Saúde</a>
+                <a href=""><i class="fas fa-bullseye"></i> Sem categoria</a>
+                <a href=""><i class="fas fa-bullseye"></i> Midias Socias</a>
+                <a href=""><i class="fas fa-bullseye"></i> Marketing & Anuncios</a>
+            </div> <!-- Categoria-artigos -->
+            <div class="clear"></div>
+        </div><!-- center -->
+        <div class="center right w66">
+        <div class="box-conteudo">
+                <div class="center left w40">
+                    <img src="../imagens/coronavirus.png" alt="">
+                    <div class="filtrar-categoria">
+                        <i class="far fa-bookmark"></i>
+                        <i class="fas fa-user-circle"></i>
+                    </div><!-- filtrar-categoria -->
+                </div><!-- center -->
+                <div class="center left w60">
+                    <h2>Ultimas noticias desde dia 26 de Abril ao coronavirus</h2>
+                    <p>Diagnóstico Como se proteger Se eu ficar doente Serviço de Saúde Fake news Perguntas Frequentes Painel Covid-19 App Coronavirus Disque Saúde 136 ...</p>
+                    <p><b>Data post: </b>12/10/2019 12:20</p>
+                    <a href="texto-blog.php">Continue lendo <i class="fas fa-arrow-down transform-icon"></i></a>
+                </div> <!-- center -->
+            </div><!-- box-conteudo -->
+            <div class="box-conteudo">
+                <div class="center left w40">
+                    <img src="../imagens/coronavirus.png" alt="">
+                    <div class="filtrar-categoria">
+                        <i class="far fa-bookmark"></i>
+                        <i class="fas fa-user-circle"></i>
+                    </div><!-- filtrar-categoria -->
+                </div><!-- center -->
+                <div class="center left w60">
+                    <h2>Ultimas noticias desde dia 26 de Abril ao coronavirus</h2>
+                    <p>Diagnóstico Como se proteger Se eu ficar doente Serviço de Saúde Fake news Perguntas Frequentes Painel Covid-19 App Coronavirus Disque Saúde 136 ...</p>
+                    <p><b>Data post: </b>12/10/2019 12:20</p>
+                    <a href="texto-blog.php">Continue lendo <i class="fas fa-arrow-down transform-icon"></i></a>
+                </div> <!-- center -->
+            </div><!-- box-conteudo --><div class="box-conteudo">
+                <div class="center left w40">
+                    <img src="../imagens/coronavirus.png" alt="">
+                    <div class="filtrar-categoria">
+                        <i class="far fa-bookmark"></i>
+                        <i class="fas fa-user-circle"></i>
+                    </div><!-- filtrar-categoria -->
+                </div><!-- center -->
+                <div class="center left w60">
+                    <h2>Ultimas noticias desde dia 26 de Abril ao coronavirus</h2>
+                    <p>Diagnóstico Como se proteger Se eu ficar doente Serviço de Saúde Fake news Perguntas Frequentes Painel Covid-19 App Coronavirus Disque Saúde 136 ...</p>
+                    <p><b>Data post: </b>12/10/2019 12:20</p>
+                    <a href="texto-blog.php">Continue lendo <i class="fas fa-arrow-down transform-icon"></i></a>
+                </div> <!-- center -->
+            </div><!-- box-conteudo --><div class="box-conteudo">
+                <div class="center left w40">
+                    <img src="../imagens/coronavirus.png" alt="">
+                    <div class="filtrar-categoria">
+                        <i class="far fa-bookmark"></i>
+                        <i class="fas fa-user-circle"></i>
+                    </div><!-- filtrar-categoria -->
+                </div><!-- center -->
+                <div class="center left w60">
+                    <h2>Ultimas noticias desde dia 26 de Abril ao coronavirus</h2>
+                    <p>Diagnóstico Como se proteger Se eu ficar doente Serviço de Saúde Fake news Perguntas Frequentes Painel Covid-19 App Coronavirus Disque Saúde 136 ...</p>
+                    <p><b>Data post: </b>12/10/2019 12:20</p>
+                    <a href="texto-blog.php">Continue lendo <i class="fas fa-arrow-down transform-icon"></i></a>
+                </div> <!-- center -->
+            </div><!-- box-conteudo -->
+            
+            <div class="nav-links">
+                <a href="">1</a> <a href="">2</a> <a href="">3</a> <a href="">...</a> <a href="">Próximo <i class="fas fa-arrow-down transform-icon"></i></a>
+            </div> <!-- nav-links -->
+        <div class="clear"></div>
+        </div><!-- center -->
+    </section>   
+   
+  </body>
+</html>
---
+++ blog-project/pages/texto-blog.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/blog.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:ital,wght@0,400;1,100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;1,100;1,200;1,500&family=Source+Sans+Pro:ital,wght@0,300;1,200&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <section>
+        <div class="center left w33">
+                <div class="artigos-em-alta">
+                    <div class="artigos-em-alta-h2">
+                        <h2><i class="fas fa-sort-amount-up-alt"></i> Artigos em Alta</h2>
+                    </div>
+                    <a href=""><i class="fas fa-arrow-circle-right fans"></i> Coronavírus: Brasil registra 58.509 casos e 4.016 mortes</a>
+                    <a href=""><i class="fas fa-arrow-circle-right fans"></i> Quatro em cada 10 brasileiros pratica atividades físicas regularmente</a>
+                    <a href=""><i class="fas fa-arrow-circle-right fans"></i> Os 10 segredos sobre como fazer Email Marketing</a>
+                    <a href=""><i class="fas fa-arrow-circle-right fans"></i> Como criar campanhas de Google Adwords</a>  
+                </div><!-- artigos-em-alta -->
+                <div class="categoria-artigos">
+                    <div class="categoria-artigos-h2">
+                        <h2><i class="far fa-bookmark"></i> Categorias</h2>
+                    </div> 
+                    <a href=""><i class="fas fa-bullseye"></i> Coronavirus</a>   
+                    <a href=""><i class="fas fa-bullseye"></i> Saúde</a>
+                    <a href=""><i class="fas fa-bullseye"></i> Sem categoria</a>
+                    <a href=""><i class="fas fa-bullseye"></i> Midias Socias</a>
+                    <a href=""><i class="fas fa-bullseye"></i> Marketing & Anuncios</a>
+                </div> <!-- Categoria-artigos -->
+                <div class="clear"></div>
+            </div><!-- center -->
+            <div class="center right w66">
+            <div class="box-conteudo">
+            <h2>Ultimas noticias desde dia 26 de Abril ao coronavirus</h2>
+            <p><b>Data post: </b>12/10/2019 12:20</p>
+            <p>Diagnóstico Como se proteger Se eu ficar doente Serviço de Saúde Fake news Perguntas Frequentes Painel Covid-19 App Coronavirus Disque Saúde 136 ...</p>
+                   
+            </div>
+            </div>
+        </section>
+  </body>
+</html>
